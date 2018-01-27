a000ea04c9ee632823e09d00316dcc1034b0cf6a
LAPTOP-KBDAS1SC\Jonathan <jonatlun@gmail.com>
2018-01-27 17:28:52 +0100

    Nicer sphere with numbers

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameBoard : MonoBehaviour {
-    public float radius;
+    public float hexRadius;
+    public float pentRadius;
     public float angle = 30f;
-    public GameObject hexagonPrefab;
-    public GameObject pentagonPrefab;
+    public GameTile hexagonPrefab;
+    public GameTile pentagonPrefab;
     public GameCamera gameCamera;
+    public GameUnit produceUnit;
 
 	// Use this for initialization
 	void Start () {
@@ -23,27 +25,37 @@ public class GameBoard : MonoBehaviour {
             if (Physics.Raycast(ray, out hit))
             {
                 GameTile tile = hit.collider.GetComponentInParent<GameTile>();
-                tile.OnClicked();
+                if (tile.currentUnit == null)
+                {
+                    GameUnit unit = GameObject.Instantiate<GameUnit>(produceUnit);
+                    tile.PlaceUnit(unit);
+                } else
+                {
+                    tile.RemoveUnit();
+                }
             }
         }
 	}
 
     void GenerateSphere()
     {
-        CreateTile(false, 36f, 0f, 90f);
+        int index = 0;
+        CreateTile(false, 36f, 0f, 90f, index);
+
         for (int i = 0; i < 5; i++)
         {
-            CreateTile(false, 0f, 72f * i, 30f);
+            index++;
+            CreateTile(true, 30f, 72f * i, 58f, index);
+        }
+        
+        for (int i = 0; i < 5; i++)
+        {
+            CreateTile(false, 0f, 72f * i, 27f);
         }
 
         for (int i = 0; i < 5; i++)
         {
-            CreateTile(true, 0f, 36f + 72f * i, 30f);
-        }
-
-        for (int i = 0; i < 5; i++)
-        {
-            CreateTile(true, 30f, 72f * i, 60f);
+            CreateTile(true, 0f, 36f + 72f * i, 31.5f);
         }
 
         for (int i = 0; i < 10; i++)
@@ -54,26 +66,28 @@ public class GameBoard : MonoBehaviour {
         CreateTile(false, 180f + 36f, 0f, -90f);
         for (int i = 0; i < 5; i++)
         {
-            CreateTile(false, 180f, 72f * i, -30f);
+            CreateTile(false, 180f, 72f * i, -27f);
         }
 
         for (int i = 0; i < 5; i++)
         {
-            CreateTile(true, 0f, 36f + 72f * i, -30f);
+            CreateTile(true, 0f, 36f + 72f * i, -31.5f);
         }
 
         for (int i = 0; i < 5; i++)
         {
-            CreateTile(true, 30f, 72f * i, -60f);
+            CreateTile(true, 30f, 72f * i, -58f);
         }
     }
 
-    private void CreateTile(bool isHexagon, float ownRot, float sideRot, float upRot)
+    private void CreateTile(bool isHexagon, float ownRot, float sideRot, float upRot, int position = 0)
     {
-        GameObject tile = GameObject.Instantiate(isHexagon ? hexagonPrefab : pentagonPrefab, transform);
+        GameTile tile = GameObject.Instantiate<GameTile>(isHexagon ? hexagonPrefab : pentagonPrefab, transform);
         tile.transform.Rotate(Vector3.right, upRot - 90f, Space.World);
         tile.transform.Rotate(Vector3.up, sideRot, Space.World);
-        tile.transform.position += tile.transform.up * radius;
+        tile.transform.position += tile.transform.up * (isHexagon ? hexRadius : pentRadius);
+        tile.position = position;
+        tile.text.text = "" + position;
         tile.transform.Rotate(0f, ownRot, 0f, Space.Self);
     }
 }
--- Assets/Scripts/GameTile.cs
+++ Assets/Scripts/GameTile.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameTile : MonoBehaviour {
+    public GameUnit currentUnit;
+    public Text text;
+    public int position;
 
     public void OnClicked()
     {
         GetComponentInChildren<Renderer>().material.color = Color.red;
     }
+
+    public void PlaceUnit(GameUnit unit)
+    {
+        unit.transform.position = transform.position;
+        unit.transform.rotation = transform.rotation;
+        currentUnit = unit;
+    }
+
+    public void RemoveUnit()
+    {
+        Destroy(currentUnit.gameObject);
+        currentUnit = null;
+    }
+
 	// Use this for initialization
 	void Start () {
 		
